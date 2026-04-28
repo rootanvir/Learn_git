54aa19c699dcd5e55f867a56adedca7696407997
rootanvir <tanvirahmed131015@gmail.com>
2026-04-28 17:22:51 +0600

    dev - about

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 #define int long long
 using namespace std;
 
+
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout <<"Hello git & github" <<'\n';
     cout <<"Hello from dev branch" <<'\n';
     cout << about() << '\n';
     return 0;
