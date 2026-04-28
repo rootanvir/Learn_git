f19f876e2306413e37f93624909360310b819078
rootanvir <tanvirahmed131015@gmail.com>
2026-04-29 01:02:31 +0600

    add -features

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
 #include "lib/about.h"
+#include "lib/add.h"
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout <<"Hello from dev branch" <<'\n';
     cout << about() << '\n';
+    cout << add(4,5) << '\n';
     return 0;
 }
