8a9f9dcb95fea0225d2781774331fde7035790ae
rootanvir <tanvirahmed131015@gmail.com>
2026-05-01 01:02:44 +0600

    reset to previous commit

---
+++ lib/about.h
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+string about() {
+    string about = "language: c++\n time:17:15:31 BST Tuesday, 28 April 2026 \n" ; 
+    return about;
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <bits/stdc++.h>
-#define int long long
-using namespace std;
+#include "lib/about.h"
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout <<"Hello git & github" <<'\n';
     cout <<"Hello from dev branch" <<'\n';
+    cout << about() << '\n';
     return 0;
 }
