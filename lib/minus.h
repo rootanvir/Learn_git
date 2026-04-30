97f21db979f9508d81dcf4e3b270d4f0ab56bd79
rootanvir <tanvirahmed131015@gmail.com>
2026-05-01 01:07:31 +0600

    s

---
+++ lib/minus.h
@@ -0,0 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+double minus(int a,int b){
+    return a - b;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include <bits/stdc++.h>
 #include "lib/about.h"
+<<<<<<< HEAD
+=======
+#include "lib/add.h"
+>>>>>>> 0fd7049b6727b771c71b1ed53ad2875ae0ea606d
 
 int32_t main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout <<"Hello from dev branch" <<'\n';
     cout << about() << '\n';
+<<<<<<< HEAD
+=======
+    cout << add(4,5) << '\n';
+>>>>>>> 0fd7049b6727b771c71b1ed53ad2875ae0ea606d
     return 0;
 }
